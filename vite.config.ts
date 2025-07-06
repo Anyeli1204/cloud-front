18b29be46fa0fa412f4279fc5618f1d537db69da
Anyeli1204 <anyeli.tamara@utec.edu.pe>
2025-07-05 20:43:17 -0500

    Eliminar proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,6 @@ import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	server: {
-		proxy: {
-			"/api": {
-				target:
-					"https://dbp-backend-springboot.wittysky-1e95b768.centralus.azurecontainerapps.io",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
-	},
 	plugins: [
 		react(),
 		tsconfigPaths(),
